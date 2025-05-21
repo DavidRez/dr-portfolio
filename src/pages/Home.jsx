1d6c1860361f36c1b7ac606f8c5f171a24c7a007
David Ramirez <davidraviles@gmail.com>
2025-05-21 16:53:09 -0700

    modified content styling, padding. added animation and seo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,22 +23,37 @@ const Home = () => {
       ref={container}
       className='section'
       style={{ minHeight: `calc(100vh - ${footerHeight}px)`,
-      paddingTop: `${window.innerWidth <= 768 ? navHeight * 1.5 : navHeight}px`,
+      paddingTop: `${window.innerWidth <= 768 ? navHeight * 2 : navHeight * 1.5}px`,
       paddingBottom: `${navHeight}px` }}
       initial={{ opacity: 0 }}
       animate={ isInView ? { opacity: 1 } : null}
       transition={{
           duration: 0.5
       }}>
-      { !loading || error ?
-        (<div className='home__container'>
-          <h1 className=''dangerouslySetInnerHTML={{ __html: data.home.header}}></h1>
-          <p>{data.home.content.text}</p> 
-        </div>) :
-        (<div className='home__container'>
-          <p>Loading</p>
-        </div>)
-        }
+      <title>{data.home.seoOverride.title}</title>
+      <meta name="description" content={data.home.seoOverride.description} />
+      <meta name="keywords" content={data.home.seoOverride.keywords} />
+      <div className='home__container'>
+        <motion.h1 
+          className='gradient-header' 
+          dangerouslySetInnerHTML={{ __html: data.home.header}}
+          initial={{ opacity: 0, y: 48 }}
+          animate={ isInView ? { opacity: 1, y: 0 } : null}
+          transition={{
+              duration: 0.5,
+              delay: 0.5
+          }}>
+        </motion.h1>
+        <motion.div 
+          dangerouslySetInnerHTML={{ __html: data.home.content.html}}
+          initial={{ opacity: 0, y: 48 }}
+          animate={ isInView ? { opacity: 1, y: 0 } : null}
+          transition={{
+              duration: 0.5,
+              delay: 0.75
+          }}>
+        </motion.div> 
+      </div>
     </motion.div>
   );
 };
