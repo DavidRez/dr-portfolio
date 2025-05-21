b1cbaf29d78317b6bc4f016c79a9a1908a53e14a
David Ramirez <davidraviles@gmail.com>
2025-05-21 12:34:20 -0700

    WorkSingle is now entire section vs just content container

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -2,72 +2,41 @@ import { useEffect, useState, useRef } from 'react';
 import { useGlobal } from '@/context/GlobalContext';
 import Spinner from '@/components/ui/Spinner/Spinner'
 import WorkSingle from '@/pages/Work/WorkSingle/WorkSingle';
-import * as motion from "motion/react-client"
-import { useInView } from "motion/react"
+import * as motion from "motion/react-client";
+import { useInView } from "motion/react";
 import "./Work.css";
 
 const Work = () => {
   const [navHeight, setNavHeight] = useState(0);
-  const { data, loading, changeColor } = useGlobal();
-  const sections = useRef([]);
-  const container = useRef(null)  
-  const isInView = useInView(container)
+  const { data, loading } = useGlobal();
+  const container = useRef(null);
+  const isInView = useInView(container);
 
   useEffect(() => {
     // get nav height
     const nav = document.getElementsByClassName('navigation')[0]
     setNavHeight(nav.clientHeight || 0);
-
-    // add Intersection Observer to sections
-    if (!loading && sections.current) {
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {  
-            if (entry.isIntersecting) {
-              changeColor(entry.target.style.background)
-            } 
-          });
-        },
-        {
-          threshold: .05, // Trigger when the entire component is visible
-          rootMargin: '0px 0px -90% 0px', // Consider the top 1% of the viewport
-        }
-      );
-
-      sections.current.forEach((item) => {
-        observer.observe(item);
-      });
-  
-      return () => {
-        sections.current.forEach((item) => {
-            // console.log('un', item)
-            // observer.unobserve(item);
-            observer.disconnect();
-        });
-      };
-    }
-  }, [sections.current, loading])
+  }, []);
 
   if (!loading) {
     return (
       <motion.div
         ref={container}
-        initial={{ opacity: 0, y: 135 }}
-        animate={ isInView ? { opacity: 1, y: 0 } : null}
+        initial={{ opacity: 0 }}
+        animate={ isInView ? { opacity: 1 } : null}
         transition={{
           duration: 0.5
         }}>
+        <title>{data.work.seoOverride.title}</title>
+        <meta name="description" content={data.work.seoOverride.description} />
+        <meta name="keywords" content={data.work.seoOverride.keywords} />
         {data.work.singleWork.map((work, i) => {
-          return <div className='section'
-            key={`work-${i}`} ref={section => sections.current[i] = section}
-            style={{ paddingTop: `${window.innerWidth <= 768 ? navHeight * 1.5 : navHeight}px`,
-            paddingBottom: `${navHeight}px`,
-            background: `${work.darkBg.hex}` }}>
-            <WorkSingle 
-              className='work'
-              reverse={ i % 2 !== 0 } 
-              props={work} /> 
-          </div> 
+          return <WorkSingle 
+            key={`work-${i}`}
+            className='work'
+            reverse={ i % 2 !== 0 } 
+            props={work} 
+            navHeight={navHeight} />
         })}
       </motion.div>
     )
--- src/pages/Work/WorkSingle/WorkSingle.jsx
+++ src/pages/Work/WorkSingle/WorkSingle.jsx
@@ -1,82 +1,161 @@
 import "./WorkSingle.css";
 import { useEffect, useRef } from "react";
-import * as motion from "motion/react-client"
-import { splitText } from "motion-plus"
-import { useInView } from "motion/react"
+import * as motion from "motion/react-client";
+import { splitText } from "motion-plus";
+import { useInView } from "motion/react";
+import { stagger } from "motion";
+import { useGlobal } from '@/context/GlobalContext';
 import animateLines from "@/utils/animateLines";
-import { stagger } from "motion"
+import Github from "@/assets/github.svg?react";
+import Live from "@/assets/live.svg?react";
+// import Quadilateral from "../../../components/ui/Quadilateral/Quadilateral";
 
-const WorkSingle = ({className, reverse, props}) => {
-  const container = useRef(null)  
-  const isInView = useInView(container)
-  
+const WorkSingle = ({ className, reverse, navHeight, props }) => {
+  const { loading, changeColor } = useGlobal();
+  const section = useRef(null);
+  const container = useRef(null);
+  const isContentInView = useInView(container); // for content
+
   useEffect(() => {
-    // Make the container visible once fonts are loaded
-    document.fonts.ready.then(() => {
-      if (!isInView) return
+    // once info has been received and ref is set
+    // change nav color to match section at the top
+    if (!loading && section.current) {
 
-      const { words } = splitText(container.current.querySelector("h1"))
-      const { lines } = splitText(container.current.querySelector("p"))
+      // add Intersection Observer when top of viewport
+      const observerTop = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {  
+            if (entry.isIntersecting) {
+              changeColor(entry.target.style.background)
+            } 
+          });
+        },
+        {
+          threshold: .05, // Trigger when the entire component is visible
+          rootMargin: '0px 0px -90% 0px', // Consider the top 10% of the viewport
+        }
+      );
 
-      animateLines(
-        words,
-        { opacity: [0, 1], y: [10, 0] },
-        'spring',
-        0,
-        1,
-        stagger(0.15)
-      )
-      animateLines(
-        lines,
-        { opacity: [0, 1], y: [20, 0] },
-        'spring',
-        0,
-        0.75,
-        stagger(0.1),
-      )
-    })
-  }, [isInView])
+      if (section.current) {
+        observerTop.observe(section.current);
+      }
+
+      // once fonts are loaded, animate
+      document.fonts.ready.then(() => {
+        if (!isContentInView) return;
+
+        const { words } = splitText(container.current.querySelector("h1"));
+        const { lines } = splitText(container.current.querySelector("p"));
+
+        animateLines(
+          words,
+          { opacity: [0, 1], y: [10, 0] },
+          "spring",
+          0,
+          1,
+          stagger(0.15)
+        );
+        animateLines(
+          lines,
+          { opacity: [0, 1], y: [20, 0] },
+          "spring",
+          0,
+          0.75,
+          stagger(0.1)
+        );
+      });
+
+      return () => {
+        observerTop.disconnect();
+      };
+    }
+  }, [isContentInView, loading, section.current]);
 
   return (
-    <div ref={container}
-      className={`${reverse ? 'work-single work-single--reverse' : 'work-single'} ${className} `} >
-      <motion.div className="work-single__media"
-        initial={{ opacity: 0, y: 45 }}
-        animate={ isInView ? { opacity: 1, y: 0 } : null}
-        transition={{
-            type: 'spring',
+    <div
+      className="work-single section"
+      ref={section}
+      style={{
+        paddingTop: `${
+          window.innerWidth <= 768 ? navHeight * 1.5 : navHeight
+        }px`,
+        paddingBottom: `${navHeight}px`,
+        background: `${props.bg}`
+      }}
+    >
+      {/* <Quadilateral /> */}
+      <div
+        ref={container}
+        className={`${
+          reverse ? "work-single__container work-single__container--reverse" : "work-single__container"
+        } ${className} `}
+      >
+        <motion.div
+          className="work-single__media"
+          initial={{ opacity: 0, y: 45 }}
+          animate={isContentInView ? { opacity: 1, y: 0 } : null}
+          transition={{
+            type: "spring",
             bounce: 0.5,
-            duration: 2
-        }}>
-        {props.hasVideo ? (
-          isInView ? <video controls>
-            <source src={props.video.url} type={props.video.mimeType} />
-          </video> :
-          <div></div>
-        ) : (
-          isInView ? <img src={props.image.url} /> : <div></div>
-        )}
-      </motion.div>
-      <div className="work-single__info">
-        <h1 className="h3">{props.title}</h1>
-        <p className="work-single__description"
-          >
-          {props.description}
-        </p>
-        <ul className="work-single__frameworks text-md">
-          {props.frameworks.map((str, j) => {
-            return <motion.li 
-              key={`framework-${j}`}
-              initial={{ opacity: 0, y: 135 }}
-              animate={ isInView ? { opacity: 1, y: 0 } : null}
-              transition={{
-                duration: 0.5,
-                delay: 0.25 * (j + .5)
-              }}>
-                {str}
-              </motion.li>;
-          })}
-        </ul>
+            duration: 2,
+          }}
+        >
+          {props.hasVideo ? (
+            isContentInView ? (
+              <video controls>
+                <source src={props.video.url} type={props.video.mimeType} />
+              </video>
+            ) : (
+              <div></div>
+            )
+          ) : isContentInView ? (
+            <img src={props.image.url} />
+          ) : (
+            <div></div>
+          )}
+        </motion.div>
+        <div className="work-single__info">
+          <h1 className="h3">{props.title}</h1>
+          <p className="work-single__description">{props.description}</p>
+          <ul className="work-single__frameworks text-md">
+            {props.frameworks.map((str, j) => {
+              return (
+                <motion.li
+                  key={`framework-${j}`}
+                  initial={{ opacity: 0, y: 135 }}
+                  animate={isContentInView ? { opacity: 1, y: 0 } : null}
+                  transition={{
+                    duration: 0.5,
+                    delay: 0.25 * (j + 0.5),
+                  }}
+                >
+                  {str}
+                </motion.li>
+              );
+            })}
+          </ul>
+          <div className='work-single__examples'>
+            {props.github ? (
+              <a
+                className="work-single__example"
+                href={props.github}
+                target="_blank"
+                aria-label={`open new tab to GitHub repo for ${props.title}`}
+              >
+                <Github />
+              </a>
+            ) : null}
+            {props.live ? (
+              <a 
+                className="work-single__example"
+                href={props.live} 
+                target='_blank' 
+                aria-label={`open new tab to live site for ${props.title}`}>
+                <Live />
+              </a>
+            ) : null}
+          </div>
+        </div>
       </div>
     </div>
   );
