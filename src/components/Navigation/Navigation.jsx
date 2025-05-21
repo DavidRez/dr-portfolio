43bb4977dbb9366ac33103f7bfb81a95e4a71b40
David Ramirez <davidraviles@gmail.com>
2025-05-21 12:35:20 -0700

    added logo with animation and class for work page

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,22 @@
 import NavigationDesktop from './NavigationDesktop';
 import NavigationMobile from './NavigationMobile';
+import { NavLink, useLocation } from 'react-router';
+import Logo from '@/assets/logo.svg?react';
 import './Navigation.css';
 import { useGlobal } from '@/context/GlobalContext';
 
 const Navigation = () => {
   const { color } = useGlobal();
+  const location = useLocation();
+  const currentRoute = location.pathname;
 
   return (
-    <header className='navigation' style={{ background: color }}>
-      <NavigationDesktop className='navigation--desktop hidden md:flex' />
-      <NavigationMobile className='navigation--mobile hidden max-md:flex' />
+    <header className='navigation' style={currentRoute.includes('work') ? { background: color } : null}>
+      <NavLink className='navigation__logo' to='/' aria-label='link to homepage'>
+          <Logo />
+      </NavLink>
+      <NavigationDesktop className='hidden md:flex' />
+      <NavigationMobile className='hidden max-md:flex' />
     </header>
   );
 };
